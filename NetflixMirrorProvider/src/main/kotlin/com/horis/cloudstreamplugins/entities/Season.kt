eca32a13b67e586fdd436db04914ccc8a1014ced
nuyuls79 <nuyulshare001@gmail.com>
2026-02-15 13:33:21 +0700

    Add files via upload

--- NetflixMirrorProvider/src/main/kotlin/com/horis/cloudstreamplugins/entities/Season.kt
+++ NetflixMirrorProvider/src/main/kotlin/com/horis/cloudstreamplugins/entities/Season.kt
@@ -1,10 +1,8 @@
 package com.horis.cloudstreamplugins.entities
 
-data class Episode(
-    val complate: String,
+data class Season(
     val ep: String,
     val id: String,
     val s: String,
-    val t: String,
-    val time: String
+    val sele: String
 )
